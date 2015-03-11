9efb0c1a2a4c066363befd0601b32bd191aef54d
Alin <admin@alin.be>
2015-03-11 11:34:59 +0100

    Add all metrics in one series

--- src/export/exportToInfluxDB.go
+++ src/export/exportToInfluxDB.go
@@ -2,29 +2,31 @@ package export
 
 import (
 	"config"
-	"nestStructure"
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"nestStructure"
 	"rest"
 	"time"
 )
 
 type influxDBStruct struct {
-	Columns [1]string    	`json:"columns"`
-	Serie    string      	`json:"name"`
-	Points  [1][1]float64 	`json:"points"`
+	Columns [3]string     `json:"columns"`
+	Serie   string        `json:"name"`
+	Points  [1][3]float64 `json:"points"`
 }
 
-func SendToInfluxDB(oneNest nestStructure.Nest,  oneConfig config.ConfigStructure) {
+func SendToInfluxDB(oneNest nestStructure.Nest, oneConfig config.ConfigStructure) {
 
 	fmt.Printf("\n %s :> Send Data to InfluxDB\n", time.Now().Format(time.RFC850))
 
 	influxDBData := influxDBStruct{}
-	influxDBData.Columns = [1]string{"Temp"}
-	influxDBData.Points = [1][1]float64{{0.0}}
-	influxDBData.Serie = "AmbientTemperature"
+	influxDBData.Columns = [3]string{"TargetTemperature", "AmbientTemperature", "Humidity"}
+	influxDBData.Points = [1][3]float64{{0.0, 0.0, 0.0}}
+	influxDBData.Serie = "NestData"
 
-	influxDBData.Points[0][0] = oneNest.GetAmbientTemperature()
+	influxDBData.Points[0][0] = oneNest.GetTargetTemperature()
+	influxDBData.Points[0][1] = oneNest.GetAmbientTemperature()
+	influxDBData.Points[0][2] = oneNest.GetHumidity()
 
 	sendPost(influxDBData, oneConfig)
 
@@ -32,27 +34,11 @@ func SendToInfluxDB(oneNest nestStructure.Nest,  oneConfig config.ConfigStructur
 	influxDBData.Points = [1][1]float64{{0.0}}
 	influxDBData.Serie = "SoftwareVersion"
 
-	influxDBData.Points[0][0] = oneNest.GetSoftwareVersion()*/
+	influxDBData.Points[0][0] = oneNest.GetSoftwareVersion()
 
 	sendPost(influxDBData, oneConfig)
 
-	influxDBData.Columns = [1]string{"Percentage"}
-	influxDBData.Points = [1][1]float64{{0.0}}
-	influxDBData.Serie = "Humidity"
-
-	influxDBData.Points[0][0] = oneNest.GetHumidity()
-
-	sendPost(influxDBData, oneConfig)
-
-	influxDBData.Columns = [1]string{"Temp"}
-	influxDBData.Points = [1][1]float64{{0.0}}
-	influxDBData.Serie = "TargetTemperature"
-
-	influxDBData.Points[0][0] = oneNest.GetTargetTemperature()
-
-	sendPost(influxDBData, oneConfig)
-
-	/*influxDBData.Columns = [1]string{"Status"}
+	influxDBData.Columns = [1]string{"Status"}
 	influxDBData.Points = [1][1]float64{{0.0}}
 	influxDBData.Serie = "Away"
 
@@ -62,14 +48,14 @@ func SendToInfluxDB(oneNest nestStructure.Nest,  oneConfig config.ConfigStructur
 }
 
 func sendPost(influxDBData influxDBStruct, oneConfig config.ConfigStructure) {
-	data, _:= json.Marshal(influxDBData)
+	data, _ := json.Marshal(influxDBData)
 
-	data = append(data,byte(']'))
-	data = append([]byte(`[`),data...)
+	data = append(data, byte(']'))
+	data = append([]byte(`[`), data...)
 
-	fullURL := fmt.Sprint("http://",oneConfig.InfluxDBServer,":",oneConfig.InfluxDBServerPort,"/db/",oneConfig.InfluxDBDatabase,"/series?u=",oneConfig.InfluxDBUsername,"&p=",oneConfig.InfluxDBPassword)
+	fullURL := fmt.Sprint("http://", oneConfig.InfluxDBServer, ":", oneConfig.InfluxDBServerPort, "/db/", oneConfig.InfluxDBDatabase, "/series?u=", oneConfig.InfluxDBUsername, "&p=", oneConfig.InfluxDBPassword)
 
 	//curl -X POST -d '[{"name":"foo","columns":["val"],"points":[[23]]}]' 'http://localhost:8086/db/nest/series?u=root&p=root'
 	oneRest := new(rest.RestHTTP)
-	oneRest.PostJSON(fullURL,data)
+	oneRest.PostJSON(fullURL, data)
 }
